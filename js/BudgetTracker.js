abaacbc27c9ed6b7d02bc89565441953a3e50bd1
jacob m <jacobamaynard@gmail.com>
2023-05-09 15:43:09 -0500

    finished budget tracker.js

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -13,7 +13,7 @@ export default class BudgetTracker {
 
     static html() {
         return `
-        <table class="budget-planner">
+        <table class="budget-tracker">
         <thead>
             <tr>
                 <th>Date</th>
@@ -71,7 +71,7 @@ export default class BudgetTracker {
     }
 
     load() {
-        const entries = JSON.parse(localStorage.getItem("budget-tracker-entries-div") || "[]");
+        const entries = JSON.parse(localStorage.getItem("budget-tracker-entries-dev") || "[]");
 
         for (const entry of entries) {
             this.addEntry(entry)
@@ -81,11 +81,35 @@ export default class BudgetTracker {
     }
 
     updateSummary() {
+        const total = this.getEntryRows().reduce((total, row) => {
+            const amount = row.querySelector(".input-amount").value;
+            const isExpense = row.querySelector(".input-type").value === "expense";
+            const modifier = isExpense ? -1 : 1
 
+            return total + (amount * modifier)
+        }, 0)
+
+        const totalFormatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(total);
+
+        this.root.querySelector(".total").textContent = totalFormatted;
     }
 
     save() {
+        const data = this.getEntryRows().map(row => {
+            return {
+                date: row.querySelector(".input-date").value,
+                description: row.querySelector(".input-description").value,
+                type: row.querySelector(".input-type").value,
+                amount: parseFloat(row.querySelector(".input-amount").value),
 
+            }
+        })
+
+        localStorage.setItem("budget-tracker-entries-dev", JSON.stringify(data));
+        this.updateSummary();
     }
 
     addEntry(entry = {}) {
@@ -93,18 +117,28 @@ export default class BudgetTracker {
 
         const row = this.root.querySelector(".entries tr:last-of-type");
 
-        row.querySelector(".input-date").value = entry.date || new Date().toISOString().replace(/T.*/, "")
+        row.querySelector(".input-date").value = entry.date || new Date().toISOString().replace(/T.*/, "");
+        row.querySelector(".input-description").value = entry.description || "";
+        row.querySelector(".input-type").value = entry.type || "income";
+        row.querySelector(".input-amount").value = entry.amount || 0;
+        row.querySelector(".delete-entry").addEventListener("click", e => {
+            this.onDeleteEntryBtnClick(e);
+        })
+        row.querySelectorAll(".input").forEach(input => {
+            input.addEventListener("change", () => this.save())
+        })
     }
 
     getEntryRows() {
-
+        return Array.from(this.root.querySelectorAll(".entries tr"))
     }
 
     onNewEntryBtnClick() {
-        this.addEntry
+        this.addEntry();
     }
 
     onDeleteEntryBtnClick(e) {
-
+        e.target.closest("tr").remove();
+        this.save()
     }
 }
